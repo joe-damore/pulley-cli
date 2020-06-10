835c726ccbeeaf7b12cb3a3998641cde83a8ed07
Joe D'Amore <joe@joedamore.me>
2020-06-10 13:37:14 -0400

    Display package metadata

--- src/packages/displayPackageInfo.js
+++ src/packages/displayPackageInfo.js
@@ -15,13 +15,111 @@ const sendRaw = (message) => {
 };
 
 /**
+ * Returns an array of strings from the given key/value pairs `keyValues`.
+ *
+ * `keyValues` should be an array of objects, each with a `key` and `value`
+ * property.
+ *
+ * @param {Array} keyValues - Array of key/value objects.
+ *
+ * @returns {Array} Array of key/value strings.
+ */
+const keyValuePairsToLines = (keyValues) => {
+  // Get max key length for formatting purposes.
+  const maxKeyLength = (() => {
+    let currentMax = 0;
+    keyValues.forEach((keyValue) => {
+      if (keyValue.key.length > currentMax) {
+        currentMax = keyValue.key.length;
+      }
+    });
+    return currentMax;
+  })();
+
+  const keyValueLines = keyValues.map((keyValue) => {
+    const key = keyValue.key;
+    const value = keyValue.value;
+
+    const paddingChar = ' ';
+    const paddingLength = maxKeyLength - key.length;
+    const paddingString = paddingChar.repeat(paddingLength);
+
+    return `${paddingString}${colors.bold(key)} : ${value}`;
+  });
+
+  return keyValueLines;
+};
+
+/**
+ * Returns an array of key/value pairs from the given metadata object.
+ *
+ * If no `allowedKeys` parameter is specified, all keys on the object are
+ * converted to key/value pairs. Otherwise, only the keys specified in
+ * `allowedKeys` are included.
+ *
+ * @param {Object} metadata - Metadata object to convert to key/value pairs.
+ * @param {Array=} allowedKeys - Strings describing keys that can be included.
+ *
+ * @returns {Array} Array of key/value objects from the given object.
+ */
+const keyValuePairsFromMetadata = (metadata, allowedKeys) => {
+  const mapFunction = (key) => {
+    return { key: key, value: metadata[key] };
+  };
+
+  const filterFunction = (key) => {
+    if (allowedKeys) {
+      return allowedKeys.includes(key);
+    }
+    return true;
+  };
+
+  return Object.keys(metadata)
+    .filter(filterFunction)
+    .map(mapFunction);
+};
+
+/**
  * Logs information about the given package.
  *
  * @param {Object} packageInstance - Package to display.
  */
 const displayPackageInfo = (packageInstance) => {
+
+  /*
+   * Array of metadata keys that, if specified, are listed under package info.
+   */
+  const allowedMetadata = (() => {
+    if (log.verbosity === log.verbosityLevels.DEBUG) {
+      // Don't whitelist metadata if DEBUG verbosity.
+      return undefined;
+    }
+    return [
+      'author',
+      'description',
+      'version',
+      'note',
+    ];
+  })();
+
+  // Always include package source with metadata display.
+  let metadata = [{
+    key: 'source',
+    value: packageInstance.source,
+  }];
+
+  // If package metadata is specified, combine it with `metadata`.
+  if (packageInstance.metadata) {
+    metadata = [
+      ...keyValuePairsFromMetadata(packageInstance.metadata, allowedMetadata),
+      ...metadata,
+    ];
+  }
+
   const lines = [
-    `${colors.bold(packageInstance.name)} from ${colors.bold(packageInstance.source)} source`,
+    `${colors.bold(packageInstance.name)}`,
+    '',
+    ...keyValuePairsToLines(metadata),
   ];
 
   sendRaw(boxen(lines.join('\n'), {
